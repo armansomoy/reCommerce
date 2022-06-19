7dae395c7835798385a0ac0b1258022f249c56cf
Arman Hossain Somoy <61237257+armansomoy@users.noreply.github.com>
2022-06-19 12:27:01 +0600

    monthly-deal complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -91,4 +91,44 @@ $(function () {
   /*if the user clicks anywhere outside the select box,
     then close all select boxes:*/
   document.addEventListener("click", closeAllSelect);
+
+  // **************** *******************//
+  $(".monthly-deal-slider").slick({
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    nextArrow: ".left",
+    prevArrow: ".right",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
 });
